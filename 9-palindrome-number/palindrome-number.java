33e7563c620c3db575f605affa88637ad58ddf40
404 NOT FOUND <151421717+ayanmomdal56@users.noreply.github.com>
2025-02-13 22:08:40 +0530

    Time: 4 ms (100.00%) | Memory: 44 MB (75.35%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
+        if(x<0){
+         return false;
         }
-        
-        int d=0;
-        int temp=x;
-
-        while (temp!=0) {
-            d = (d*10)+(temp%10);
-            temp = temp / 10;
+        int d=0,temp=x;
+        while(temp>0){
+            d=d*10+temp%10;
+            temp/=10;
         }
-
-        return d == x;
+        return d==x;
     }
 }
