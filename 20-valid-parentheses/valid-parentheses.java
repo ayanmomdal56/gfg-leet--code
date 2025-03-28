6c1b1ba45b18e19eec7de7ff7debe6af9fc9ffff
404 NOT FOUND <151421717+ayanmomdal56@users.noreply.github.com>
2025-03-28 12:04:14 +0530

    Time: 2 ms (97.58%) | Memory: 41.7 MB (65.46%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,18 +1,25 @@
 class Solution {
-    public boolean isValid(String s) { 
-        Stack<Character> stack = new Stack<>();
-  for (char ch : s.toCharArray()) {
-            if (ch == '('|| ch=='{'|| ch=='['){
-        stack.push(ch);
-        }
-        else if (stack.isEmpty() ||
-           (ch == ')' && stack.pop() != '(') || 
-                      (ch == '}' && stack.pop() != '{') || 
-                      (ch == ']' && stack.pop() != '[')) 
-                return false;
-        
+    public boolean isValid(String s) {
 
-  }
-  return stack.isEmpty();
+Stack<Character> stack= new Stack<>();
+     for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+         if(ch=='[' || ch=='{' || ch=='(')
+    stack.push(ch);
+    else{
+        if(stack.isEmpty()){
+            return false;
+        }
+        char top=stack.pop();
+                 if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false; 
+                }
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
+    
