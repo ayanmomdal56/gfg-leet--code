db0a5dde8eca2f0a1897a18c425b14d7bac0b69c
404 NOT FOUND <151421717+ayanmomdal56@users.noreply.github.com>
2025-04-25 12:10:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.java
@@ -80,28 +80,25 @@ class Node
 class Solution {
     // Function to check whether the list is palindrome.
     static boolean isPalindrome(Node head) {
-        // Your code here
-        
-        Stack<Integer> stack = new Stack<>();
-        Node temp=head;
-        
-        
-        while(temp!=null){
-      stack.push(temp.data);
-     temp=temp.next;
-    }
-    
-    
- temp = head;
-        while (temp!=null) {
-            if(temp.data!=stack.pop()) 
-            return false;
+        ArrayList <Integer> list= new ArrayList<>();
+        Node temp = head;
+        while(temp!=null)
+        {
+            list.add(temp.data);
             temp=temp.next;
+            
         }
-       return true;
         
-    
-
-     
+        
+        int i=0 ,j=list.size()-1;
+        while(i<j){
+             if (!list.get(i).equals(list.get(j))) {
+                return false;
+        }
+        
+        i++;
+        j--;
+        }
+        return true;
     }
 }
