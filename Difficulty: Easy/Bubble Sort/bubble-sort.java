38caf1108ff4a9f9f92646ba6820962c427a4b03
Ayan Mondal <151421717+ayanmomdal56@users.noreply.github.com>
2025-08-06 11:37:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bubble Sort/bubble-sort.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public void bubbleSort(int[] arr) {
+        for (int i=0; i<arr.length-1;i++) {
+            for (int j=1;j<arr.length-i;j++) {
+                if(arr[j] <arr[j-1]) {
+                
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j - 1]=temp;
+                }
+            }
+        }
+    }
+}
