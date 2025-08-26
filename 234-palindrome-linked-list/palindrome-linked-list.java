5806e1d15f61368b1cc134376b4698585c53acfc
Ayan Mondal <151421717+ayanmomdal56@users.noreply.github.com>
2025-08-26 11:49:30 +0530

    Time: 4 ms (83.83%) | Memory: 68.6 MB (38.87%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,23 +10,53 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stack = new Stack<>();
-         ListNode temp = head;
+
+if(head==null ){
+    return true;
+
+}
+if(head.next==null){
+    return true;
+
+}
+
+ListNode fast=head;
+ListNode slow= head;
 
 
-         while(temp!=null){
-            stack.push(temp.val);
-            temp=temp.next;
-         }
-temp=head;
-       while(temp!=null){
-        if(temp.val!=stack.pop()){
-            return false;
-        }
-temp=temp.next;
-       
-       }
-       return true;
-        
+while(fast!=null && fast.next!=null){
+slow=slow.next;
+fast=fast.next.next;
+
+}
+ListNode prev=null;
+ListNode cur=slow;
+
+while(cur!=null){
+ListNode n=cur.next;
+cur.next=prev;
+prev=cur;
+cur=n;
+
+}
+
+
+ListNode first=head;
+ListNode sec=prev;
+
+
+
+while(sec!=null){
+if(first.val!=sec.val){
+return false;
+}
+first = first.next;
+sec=sec.next;
+}
+
+
+
+
+        return true;
     }
 }
