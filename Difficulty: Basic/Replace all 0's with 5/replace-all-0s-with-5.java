16975ebc05f7393fb979c11ec84830cccfb12655
Ayan Mondal <151421717+ayanmomdal56@users.noreply.github.com>
2025-08-31 10:04:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Replace all 0's with 5/replace-all-0s-with-5.java
@@ -0,0 +1,11 @@
+class Solution {
+    int convertfive(int num) {
+      String str= String.valueOf(num);
+      
+      str=str.replace('0','5');
+      
+      
+      return Integer.parseInt(str);
+        
+    }
+}
