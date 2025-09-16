0b0481d4d93ce413303588c8c45dd1c4457c64db
Ayan Mondal <151421717+ayanmomdal56@users.noreply.github.com>
2025-09-17 00:09:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -1,53 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
 import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().majorityElement(arr));
+class Solution 
+{
+    int majorityElement(int arr[]) 
+    {
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = arr.length;
+        for (int num : arr) 
+        {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.get(num) > n / 2)
+            return num; 
         }
-    }
-}
-// } Driver Code Ends
-
-
-class Solution {
-    static int majorityElement(int arr[]) {
-        int c = 0;
-        int ele = -1; 
-        
-        for (int i = 0; i < arr.length; i++) {
-            if (c == 0) {
-                ele = arr[i]; 
-                c = 1;          
-            } else if (arr[i] == ele) {
-                c++; 
-            } else {
-                c--; 
-            }
-        }
-        int cnt1 = 0;
-        for (int j = 0; j < arr.length; j++) {
-            if (arr[j] == ele) {
-                cnt1++;  
-            }
-        }
-
-
-        if (cnt1 > (arr.length / 2)) {
-            return ele;
-        }
-        
-        
         return -1;
     }
 }
